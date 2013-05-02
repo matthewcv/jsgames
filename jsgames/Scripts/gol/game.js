8097064865e0249e53d5d490fb5f53e2ee4fe940
matthewcv <matthew@matthewcv.com>
2013-05-02 09:58:08 -0500

    game logic and UI stuff

--- jsgames/Scripts/gol/game.js
+++ jsgames/Scripts/gol/game.js
@@ -2,15 +2,111 @@
 
     window.gol = {};
 
-    window.gol.Game = function(rows, cols, draw) {
+    function refreshGrid(grid, alive) {
+        //grid is the whole universe with living and dead cells
+        //alive is the coordinates of living cells.  All others will be dead.
+
+        //make everything dead
+        for (var y = 0; y < grid.length; y++) {
+            var row = grid[y];
+            for (var x = 0; x < row.length; x++) {
+                row[x] = false;
+            }
+        }
+        
+        //make the alive ones alive
+        for (var i = 0; i < alive.length; i++) {
+            var x = alive[i][0], y = alive[i][1];
+            grid[y][x] = true;
+        }
+    }
+
+
+    window.gol.Game = function(rows, cols, init) {
         this.rows = rows;
         this.cols = cols;
-        this.draw = draw;
+        this.grid = new Array(rows);
+        for (var i = 0; i < rows; i++) {
+            this.grid[i] = new Array(cols);
+        }        
+        
+        //set the initial live cells
+        refreshGrid(this.grid, init);
+        
 
-        this.setRandomPattern = function(cellCount) {
-            
+        this.life = function() {
+            var newliving = [];
+            for (var y = 0; y < this.rows; y++) {
+                for (var x = 0; x < this.cols; x++) {
+                    var ln = this.countLivingNeighbors(x, y);
+                    //am I dead or alive
+                    var alive = this.grid[y][x];
+                    if (alive && (ln === 2 || ln === 3)) {
+                        newliving.push([x, y]); //stays alive
+                    }
+                    if (!alive && ln === 3) {
+                        newliving.push([x, y]);//becomes alive
+                    }
+                    //all others are dead
+                }
+
+            }
+            refreshGrid(this.grid, newliving);
+            return this.grid;
+
         };
+
+        this.countLivingNeighbors = function(x, y) {
+            var living = 0;
+            //look for neighbors in this order N, NE, E, SE, S, SW, W, NW
+            //N neighbor
+            if (y > 0 && this.grid[y - 1][x]) living++;
+            //NE neighbor
+            if (y > 0 && x < this.cols - 1 && this.grid[y - 1][x + 1]) living++;
+            //E neighbor
+            if (x < this.cols - 1 && this.grid[y][x + 1]) living++;
+            //SE neighbor
+            if (x < this.cols - 1 && y < this.rows - 1 && this.grid[y + 1][x + 1]) living++;
+            //S neighbor
+            if (y < this.rows + 1 && this.grid[y + 1][x]) living++;
+            //SW neighbor
+            if (y < this.rows + 1 && x > 0 && this.grid[y + 1][x - 1]) living++;
+            //W neighbor
+            if (x > 0 && this.grid[y][x - 1]) living++;
+            //NW neighbor
+            if (x > 0 && y > 0 && this.grid[y - 1][x - 1]) living++;
+
+            return living;
+        };
+
+
     };
 
+
+    /*
+
+    Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+    Any live cell with two or three live neighbours lives on to the next generation.
+    Any live cell with more than three live neighbours dies, as if by overcrowding.
+    Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+   x ->  0 1 2 3 4 5 6 7 8 9 ......
+ y
+ | 
+ ▼      
+ 0       0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+ 1       0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+ 2       0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+ 3       0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+ 4       0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+ 5       0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+ 6       0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+ 7       0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+ 8       0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+ 9       0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+    
+    
+    
+    */
 
 })();
--- jsgames/Scripts/gol/ui.js
+++ jsgames/Scripts/gol/ui.js
@@ -25,17 +25,49 @@
     var cols = 40, rows = 30, cellSize = 3.5, cellSep = .7, cellRadius=.8, bgcolor = "#F2F2F2", deadcolor = "#FFFFFF", alivecolor = "#04BA6B";
     
     function Cell(stage,x,y) {
-        this.isAlive = false;
+        this._isAlive = false;
+        this._changed = true;
+        
         this.shape = new createjs.Shape();
         this.stage = stage;
-        this.color = this.isAlive ? alivecolor: deadcolor;
-        
+        this.x = x;
+        this.y = y;
+
         this.stage.addChild(this.shape);
         this.shape.x = mm((x * cellSize) + (x * cellSep) + cellSep);
         this.shape.y = mm((y * cellSize) + (y * cellSep) + cellSep);
+        var that = this;
         
-        this.draw = function() {
-            this.shape.graphics.beginFill(this.color).drawRoundRect(0, 0, mm(cellSize), mm(cellSize), mm(cellRadius));
+        this.shape.addEventListener('click', function(ev) {
+            that.toggleAlive();
+        });
+
+        this.toggleAlive = function() {
+            that.isAlive(!that.isAlive());
+        };
+
+        this.isAlive = function(val) {
+            if (val === undefined) {
+                return this._isAlive;
+            }
+            if (val !== this._isAlive) {
+                this._isAlive = val;
+                this._changed = true;
+            }
+        };
+
+        this.getColor = function() {
+            return this.isAlive() ? alivecolor : deadcolor;
+        };
+        
+        this.draw = function () {
+            if (this._changed) {
+                this.shape.graphics.clear();
+                this.shape.graphics.beginFill(this.getColor()).drawRoundRect(0, 0, mm(cellSize), mm(cellSize), mm(cellRadius));
+                this._changed = false;
+                return true;
+            }
+            return false;
         };
         
 
@@ -45,36 +77,79 @@
         this.stage = new createjs.Stage("golcanvas");
         this.background = new createjs.Shape();
         this.cells = [];
-        this.width = null;
-        this.height = null;
-        
+        this.width = null;//the width of the canvas in mm
+        this.height = null;//the height of the canvas in mm
+        this.game = null;
+
+        this._clickTime = createjs.Ticker.getTime();
+
         var that = this;
+
 
         this.init = function() {
             this.width = mm((cols * cellSize) + (cols * cellSep) + cellSep);
             this.height = mm((rows * cellSize) + (rows * cellSep) + cellSep);
             $("#golcanvas").attr({ "width": this.width, "height": this.height });
+            $("#start-game").click(function(ev) {
+                startGame.apply(that);
+                return false;
+            });
 
             this.background.graphics.beginFill(bgcolor).rect(0, 0, this.width, this.height);
             this.stage.addChild(this.background);
 
             initCells.apply(this);
-
-            this.stage.update();
+            createjs.Ticker.setFPS(60);
+            createjs.Ticker.addEventListener('tick', function (ev) {
+                
+                updateStage.apply(that);
+            });
         };
 
 
         this.init();
     }
 
+    function getLivingCellCoords() {
+        var alive = [];
+        for (var y = 0; y < rows; y++) {
+            for (var x = 0; x < cols; x++) {
+                var c = this.cells[y][x];
+                if (c.isAlive()) {
+                    alive.push( [c.x, c.y]);
+                }
+            }
+        }
+        return alive;
+    }
+
+    function startGame() {
+        var init = getLivingCellCoords.apply(this);
+        this.game = new gol.Game(rows,cols, init);
+    }
+    
+    function updateStage() {
+        var update = false;
+        for (var y = 0; y < rows; y++) {
+            for (var x = 0; x < cols; x++) {
+                var cu = this.cells[y][x].draw();
+                if (!update && cu) {
+                    update = true;
+                }
+            }
+        }
+        if (update) {
+            this.stage.update();
+        }
+    }
+
 
     function initCells() {
-        for (var i = 0; i < rows; i++) {
+        for (var y = 0; y < rows; y++) {
             this.cells.push([]);
-            for (var j = 0; j < cols; j++) {
-                var cell = new Cell(this.stage, j, i);
-                this.cells[i].push(cell);
-                cell.draw();
+            for (var x = 0; x < cols; x++) {
+                var cell = new Cell(this.stage, x, y);
+                this.cells[y].push(cell);
             }
         }
     }
